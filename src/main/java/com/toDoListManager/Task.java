20a1b839feaa8bb73da9de879c1656256221e575
lauraawiseman <laurawiseman_90@hotmail.com>
2025-06-25 17:20:14 -0230

    Added code for User and Task classes

--- src/main/java/com/toDoListManager/Task.java
+++ src/main/java/com/toDoListManager/Task.java
@@ -1,3 +1,32 @@
 package com.toDoListManager;
 
-public class Task {}
+public class Task {
+    String description;
+    boolean isCompleted;
+    Task next;
+
+    // Constructors
+    public Task(String description){
+        this.description = description;
+        this.isCompleted = false; // Setting false initially for new tasks
+        this.next = null;
+    }
+
+    // Getters
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted(){
+        return isCompleted;
+    }
+
+    public void markCompleted(){
+        this.isCompleted = true;
+    }
+
+    @Override
+    public String toString(){
+        return (isCompleted ? "[x] " : "[ ] ") + description;
+    }
+}
--- src/main/java/com/toDoListManager/User.java
+++ src/main/java/com/toDoListManager/User.java
@@ -1,3 +1,30 @@
 package com.toDoListManager;
 
-public class User {}
+public class User {
+    private String name;
+    private TaskList taskList;
+
+    public User(String name){
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addTask(String description){
+        Task newTask = new Task(description);
+        taskList.addTask(newTask);
+    }
+
+    public void markTaskCompleted(int index){
+        taskList.markTaskCompleted(index);
+    }
+
+    public void printTasks(){
+        System.out.println("Tasks for " + name + ":");
+        taskList.printTasks();
+        System.out.println();
+    }
+}
